a1d64dc19b837c50b96b5cf71fd33d7ccf7558da
cccccroge <pooopooo543@gmail.com>
2020-11-02 14:47:31 +0800

    Setup router environment

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,27 @@
 import '../stylesheets/App.scss';
 import React from 'react';
+import BrowsePanel from './BrowsePanel';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <BrowsePanel />
+        <div className="page">
+          <Switch>
+            <Route path="/test1">
+              <div>HIHI</div>
+            </Route>
+            <Route path="/test2">
+              <div>YOYO</div>
+            </Route>
+            <Route path="/">
+              <div>Home</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/BrowsePanel.tsx
@@ -0,0 +1,13 @@
+import '../stylesheets/BrowsePanel.scss';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BrowsePanel = () => {
+  return (<div id="browse-panel">
+    <Link className="link" to="/">Home</Link>
+    <Link className="link" to="/test1">test1</Link>
+    <Link className="link" to="/test2">test2</Link>
+  </div>);
+};
+
+export default BrowsePanel;
