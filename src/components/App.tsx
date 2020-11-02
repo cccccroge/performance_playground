01a47a594f865360e9f0c15a17faf9dd83aa025e
cccccroge <pooopooo543@gmail.com>
2020-11-02 13:32:12 +0800

    Initial commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import '../stylesheets/App.scss';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
